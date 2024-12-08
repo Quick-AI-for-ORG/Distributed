51fee3f20e0ad94923dd584ef42098d80fda1d66
Navanivous <Khaled2003yehia@gmail.com>
2024-12-08 20:28:39 +0200

    IPDecoder tany

--- Algorithm/IPDecoder.py
+++ Algorithm/IPDecoder.py
@@ -2,7 +2,13 @@ import urllib
 def getIP(context):
     peer = context.peer()
     decoded_peer = urllib.parse.unquote(peer)
-    ip = client_ip = decoded_peer.split('[')[1].split(']')[0]
+    
+    if decoded_peer.startswith('ipv6:'):
+        ip = decoded_peer.split('[')[1].split(']')[0]  # Extract part between [ and ]
+    elif decoded_peer.startswith('ipv4:'):
+        ip = decoded_peer.split(':')[1]
+    else:
+        ip = 'unknown'
     port = decoded_peer.split(']')[1].split(':')[1]
     print(f"ip: {ip} port: {port}")
     return ip, port
